ec05e175fd3b89a3af48c3f48349b6a9e4dcf14b
Anil maisuriya <maisuriya_anil@yahoo.in>
2022-04-10 23:26:39 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [email, setemail] = useState("");
+  let [passward, setpassward] = useState("");
+  let [femail, setfemail] = useState("");
+  let [fpassward, setfpassward] = useState("");
+
+  function channgehandler(e) {
+    setemail(e.target.value);
+  }
+  function channgehandler1(e1) {
+    setpassward(e1.target.value);
+  }
+
+  function submithandler(e) {
+    e.preventDefault();
+    setfemail(email);
+    setfpassward(passward);
+    setemail("");
+    setpassward("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <form onSubmit={submithandler}>
+      <fieldset className="main">
+        <lable>Email:-</lable>
+        <input
+          type="email"
+          className="input"
+          onChange={channgehandler}
+          value={email}
+        />
+        <br />
+        <lable>Passward:-</lable>
+        <input
+          type="passward"
+          className="input"
+          onChange={channgehandler1}
+          value={passward}
+        />
+        <br />
+        <input type="submit" className="input input1" value="Login" />
+      </fieldset>
+      <div className="data">
+        <h4>data</h4>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>Email:-</span>
+          {femail}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <span>Passward:-</span>
+          {fpassward}
+        </p>
+      </div>
+    </form>
   );
 }
 
